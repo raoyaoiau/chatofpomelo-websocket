330b0d42e0dc102c090686b68c7b779fa75e5fc1
xiaxu <gongzhuiau@163.com>
2019-05-23 19:44:36 +0800

    2232323232323232323443

--- 00.start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-start "master-server" 1.master.bat
-start "gate-server" 2.gate.bat
-start "connector-server" 3.connector.bat
-start "chat-server" 4.chat.bat
-start "web-server" client.bat
-REM start "stop_gameserver" stop_gameserver.bat
